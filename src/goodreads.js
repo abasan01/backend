c144984312da309a071dea0d7739465e148c08f5
abasan01 <andrija3000@gmail.com>
2023-07-15 14:13:25 +0200

    getone riješen

--- src/goodreads.js
+++ src/goodreads.js
@@ -5,15 +5,27 @@ import {
 import puppeteer from "puppeteer";
 
 
+
 async function getReads(query) {
 
     try {
+        console.log("Spajanje na goodreads")
         const responseSearch = await axios.get(`https://www.goodreads.com/search?q=${query}`);
         let $ = load(responseSearch.data)
-        const bookTitle = $(".bookTitle:first");
-        const bookLink = "https://www.goodreads.com" + bookTitle.attr("href")
+        const bookTitle = $(".bookTitle:first").attr("href");
+        const authorName = $(".authorName:first").text()
+        //console.log("authorName: ", authorName)
+        if (bookTitle === undefined) {
+            throw new Error("Knjiga nije nađena!");
+        }
+        const bookLink = "https://www.goodreads.com" + bookTitle
+        //console.log("bookLink: ", bookLink)
 
-        const browser = await puppeteer.launch();
+
+        console.log("Očitavanje stranice")
+        const browser = await puppeteer.launch({
+            headless: "new"
+        });
         const page = await browser.newPage();
         await page.goto(bookLink);
 
@@ -23,12 +35,33 @@ async function getReads(query) {
 
         const htmlContent = await page.content();
         $ = load(htmlContent)
-        const bookImage = $(".BookCover__image img")
-        console.log("bookCover: ", bookImage)
-        const imageUrl = bookImage.attr("src")
-        console.log("imageUrl: ", imageUrl)
 
-        return imageUrl
+        const yearPublic = $('.FeaturedDetails [data-testid="publicationInfo"]').text()
+        let splitted = yearPublic.split(",")
+
+        let desc = $(".DetailsLayoutRightParagraph__widthConstrained").text()
+        //console.log(desc)
+
+        const bookImage = $(".BookCover__image img")
+        const imageUrl = bookImage.attr("src")
+        //console.log("imageUrl: ", imageUrl)
+        //console.log("bookCover: ", bookImage)
+
+        const genre = $('.BookPageMetadataSection__genres .Button__labelItem:first').text()
+        //console.log("genre", genre)
+
+        let pagesArray = $(".BookDetails").text()
+
+        let pages = pagesArray.split(" ")
+
+        return {
+            imageUrl: imageUrl,
+            year: splitted[1].trim(),
+            description: desc,
+            genre: genre,
+            author: authorName,
+            pages: pages[0]
+        }
     } catch (error) {
         console.error("Error retrieving from goodreads:", error);
     }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import storage from './memory_storage'
-import axios from 'axios'
 import connect from "./db.js"
-import {
-    Cheerio
-} from 'cheerio'
-import {
-    getWiki
-} from './wiki'
 import {
     getReads
 } from "./goodreads"
@@ -19,11 +12,13 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
+//getReads("Oliver Twist").then((result) => console.log(result))
+
+
 
 app.post('/upload', (req, res) => {
 
     console.log(req.body)
-
 
     let upload = req.body
 
@@ -34,32 +29,89 @@ app.post('/upload', (req, res) => {
     res.json(upload)
 });
 
-
-app.get('/knjige', async (req, res) => {
+app.get('/setall', async (req, res) => {
     let db = await connect()
 
     let cursor = await db.collection("knjige").find().sort({
-        naslov: 1
+        title: 1
     })
 
     let data = []
-
     try {
         const docs = await cursor.toArray();
 
         for (const doc of docs) {
-            if (doc.hasOwnProperty("imageUrl")) {
-                data.push(doc);
-            } else {
-                let imageUrl = await getReads(doc.naslov)
+            const readVars = await getReads(doc.title)
+            if (readVars) {
 
                 await db.collection("knjige").updateOne({
                     _id: doc._id
                 }, {
                     $set: {
-                        imageUrl: imageUrl
+                        imageUrl: readVars.imageUrl,
+                        year: readVars.year,
+                        description: readVars.description,
+                        genre: readVars.genre,
+                        author: readVars.author,
+                        pages: readVars.pages
                     }
                 });
+            }
+            data.push(doc);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    }
+    console.log("Done!")
+    res.json(data);
+
+});
+
+app.get('/setmissing', async (req, res) => {
+    let db = await connect()
+
+    let id = '64b130911bbf0c4d6b7e3c08'
+
+    var mongo = require('mongodb');
+    var o_id = new mongo.ObjectId(id);
+
+    let cursor = await db.collection("knjige").find().sort({
+        title: 1
+    })
+
+    let data = []
+    try {
+        const docs = await cursor.toArray();
+
+        for (const doc of docs) {
+            if (!doc.hasOwnProperty("imageUrl") || !doc.hasOwnProperty("year") || !doc.hasOwnProperty("description") ||
+                !doc.hasOwnProperty("genre") || !doc.hasOwnProperty("author") || !doc.hasOwnProperty("pages")) {
+                const readVars = await getReads(doc.title)
+
+                if (readVars) {
+
+                    await db.collection("knjige").updateOne({
+                        _id: doc._id
+                    }, {
+                        $set: {
+                            imageUrl: readVars.imageUrl,
+                            year: readVars.year,
+                            description: readVars.description,
+                            genre: readVars.genre,
+                            author: readVars.author,
+                            pages: readVars.pages
+                        }
+                    });
+                }
+
+                data.push(doc);
+
+
+            } else {
+                console.log(doc._id == o_id)
 
                 data.push(doc);
             }
@@ -70,27 +122,74 @@ app.get('/knjige', async (req, res) => {
             error: "Internal Server Error"
         });
     }
+    console.log("Done!")
     res.json(data);
 
 });
 
+app.get('/knjige', async (req, res) => {
+    let searchTerm = req.query.search
+    let search = {
+        $and: [{}]
+    }
+    if (searchTerm) {
+        let searchArray = String(searchTerm).split(" ")
 
-app.get('/knjige_memory', (req, res) => {
-
-    let search = req.query.search
-    let data = storage.knjige
-
-    console.log("search: ", search)
-
-    if (search) {
-        data = data.filter(doc => {
-            // @ts-ignore
-            return doc.naslov.indexOf(search) >= 0
+        searchArray.forEach((term) => {
+            let termReg = RegExp(term, "i")
+            let or = {
+                $or: [{
+                    title: termReg
+                }, {
+                    author: termReg
+                }]
+            }
+            search.$and.push(or)
         })
     }
+    try {
+        let db = await connect();
 
-    res.json(data)
+        let cursor = db.collection("knjige").find(search).sort({
+            title: 1
+        });
+
+        let documents = await cursor.toArray();
+        res.json(documents);
+
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    }
 });
+
+app.get("/knjige/:id", async (req, res) => {
+
+
+    try {
+        let id = req.params.id
+
+        var mongo = require('mongodb');
+        var o_id = new mongo.ObjectId(id);
+
+        console.log(o_id)
+
+        let db = await connect();
+
+        let data = await db.collection("knjige").findOne({
+            _id: o_id
+
+        })
+        res.json(data)
+
+    } catch (e) {
+        console.error("Error:", e)
+    }
+
+
+})
 
 
 

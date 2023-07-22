175b8482daa415c1d1568b94a230ed8787e540c3
abasan01 <andrija3000@gmail.com>
2023-07-22 09:25:12 +0200

    muke sa env

--- src/db.js
+++ src/db.js
@@ -2,6 +2,8 @@ import {
     MongoClient
 } from "mongodb"
 import "dotenv/config";
+
+console.log(process.env.JWT_SECRET)
 const connection_string = `mongodb+srv://andrija3000:admin@knjigogram.m2asola.mongodb.net/?retryWrites=true&w=majority`;
 
 let client = new MongoClient(connection_string);
